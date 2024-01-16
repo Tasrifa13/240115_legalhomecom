a3d979eda276f321173fef3c793f71f51b9d5707
itsssrafy <mrafi.rafi3011@gmail.com>
2024-01-15 17:11:46 -0800

    saya habis buat route ya

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('kuda', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/legality', [App\Http\Controllers\HomeController::class, 'legality'])->name('legality');
+Route::get('/conntact', [App\Http\Controllers\HomeController::class, 'conntact'])->name('conntact');
