5b506ee48896f8a695f72ff337fe9ef4829b753c
Ryu9145 <mohgiansaputra@gmail.com>
2024-01-22 09:52:21 +0700

    legalitas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,30 +62,4 @@
 </div>
 </section>
 <!-- End Hero -->
-
-<!-- ======= Skills Section ======= -->
-<section id="skills" class="skills">
-      <div class="container" data-aos="fade-up">
-
-        <div class="row">
-          <div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="200">
-            <img src="assets/img/img2.jpg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="200">
-            <h3>Voluptatem dignissimos provi quasi corporis voluptates</h3>
-            <p data-aos="fade-left" data-aos-delay="200">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam itaque odio nostrum, distinctio harum vel. Ducimus quibusdam delectus nesciunt. Veniam dolorem dolorum animi ipsum aperiam sapiente unde, et sit error.
-            </p>
-            <p data-aos="fade-left" data-aos-delay="200">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam itaque odio nostrum, distinctio harum vel. Ducimus quibusdam delectus nesciunt. Veniam dolorem dolorum animi ipsum aperiam sapiente unde, et sit error.
-            </p>
-            <p data-aos="fade-left" data-aos-delay="200">
-              do eiusmod tempor incididunt ut labore et dolore Veniam dolorem dolorum animi ipsum aperiam sapiente unde, et sit error. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur aliquam praesentium temporibus fuga, non, excepturi odit facilis hic similique corporis veritatis ea commodi perspiciatis dicta voluptatum, beatae quod dignissimos deserunt!
-            </p>
-          </div>
-        </div>
-
-      </div>
 @endsection
