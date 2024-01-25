72e06515b7df14c3f3d11824cee544f20f1ab12d
fitrianingsih7 <fn76428@gmail.com>
2024-01-25 15:15:27 +0800

    jkjkj

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -17,4 +17,10 @@
       <button type="submit">Submit</button>
     </form>
   </section>
+
+  <section>
+    <div class="col-lg">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.345511354345!2d119.87275468300882!3d-0.8820804999999912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d8bed0676cef417%3A0xd7bb942ffdc70a95!2sRevive%20Distro!5e0!3m2!1sen!2sid!4v1706159480413!5m2!1sen!2sid" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</section>
 @endsection
